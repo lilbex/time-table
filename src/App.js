00fefd03d09cf53b52b71435aa2a26eea871b786
lilbex <47287618+lilbex@users.noreply.github.com>
2021-07-31 19:01:30 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import AllSubject from './components/timetable/days/AllSubject';
-// import SingleClass from './components/timetable/singleclass/SingleClass';
+import SingleClass from './components/timetable/singleclass/SingleClass';
+import Navigation from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <header className="App-header"> 
-      <AllSubject />
+      
       Welcome to automatic time-table generator website
       </header>
     </div>
--- src/components/timetable/days/AllSubject.js
+++ src/components/timetable/days/AllSubject.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import { Table } from 'react-bootstrap';
-import Monday from './Monday';
-import Tuesday from './Tuesday';
-import Wednesday from './Wednesday';
-import Thursday from './Thursday';
-import Friday from './Friday';
+import React from "react";
+import { Table } from "react-bootstrap";
+import Monday from "./Monday";
+import Tuesday from "./Tuesday";
+import Wednesday from "./Wednesday";
+import Thursday from "./Thursday";
+import Friday from "./Friday";
 
 export default function AllSubject() {
   return (
-    
-<Table striped bordered hover size="sm" responsive style={{ borderColor:'black', backgroundColor:'white', }}>
-  <tr>
-    <td colSpan="7"><h3>Time-table for the whole school</h3></td>
-  </tr>
-  <tbody>
-    <Monday />
-    <Tuesday />
-    <Wednesday />
-    <Thursday />
-    <Friday />
-  </tbody>
-</Table>
-  )
+    <Table
+      striped
+      bordered
+      hover
+      size="sm"
+      responsive
+      style={{ borderColor: "black", backgroundColor: "white" }}
+    >
+      <tr>
+        <td colSpan="7">
+          <h3>Time-table for the whole school</h3>
+        </td>
+      </tr>
+      <tbody>
+        <Monday />
+        <Tuesday />
+        <Wednesday />
+        <Thursday />
+        <Friday />
+      </tbody>
+    </Table>
+  );
 }
